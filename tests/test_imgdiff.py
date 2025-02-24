6b51464fff80f0c1063aef01991fa267c4706c0f
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2025-02-24 18:58:17 +0000

    pre-commit

--- tests/test_imgdiff.py
+++ tests/test_imgdiff.py
@@ -14,7 +14,7 @@ THISDIR = Path(__file__).resolve().parent
 from blendmodes.imgdiff import image_diff, is_equal, is_x_diff
 
 
-def test_identical_images():
+def test_identical_images() -> None:
 	img = Image.new("RGB", (100, 100), "red")
 	assert image_diff(img, img) == 0.0
 	assert is_equal(img, img)
@@ -24,7 +24,7 @@ def test_identical_images():
 	assert imgcompare.image_diff_percent(img, img) == image_diff(img, img)
 
 
-def test_completely_different_images():
+def test_completely_different_images() -> None:
 	img1 = Image.new("RGB", (100, 100), "white")
 	img2 = Image.new("RGB", (100, 100), "black")
 	assert image_diff(img1, img2, compare_mode="RGB") == 100
@@ -35,7 +35,7 @@ def test_completely_different_images():
 	assert imgcompare.image_diff_percent(img1, img2) == image_diff(img1, img2, "RGB")
 
 
-def test_red_blue_rgb():
+def test_red_blue_rgb() -> None:
 	img1 = Image.new("RGB", (100, 100), "red")
 	img2 = Image.new("RGB", (100, 100), "blue")
 	assert is_x_diff(img1, img2, compare_mode="RGB", cmp_diff=33, tolerance=1)
@@ -44,7 +44,7 @@ def test_red_blue_rgb():
 	# assert imgcompare.image_diff_percent(img1, img2) == image_diff(img1, img2, compare_mode="RGB")
 
 
-def test_red_blue_l():
+def test_red_blue_l() -> None:
 	img1 = Image.new("RGB", (100, 100), "red")
 	img2 = Image.new("RGB", (100, 100), "blue")
 	assert is_x_diff(img1, img2, compare_mode="L", cmp_diff=18, tolerance=1)
@@ -53,14 +53,14 @@ def test_red_blue_l():
 	# assert imgcompare.image_diff_percent(img1, img2) == image_diff(img1, img2, compare_mode="L")
 
 
-def test_different_size_images():
+def test_different_size_images() -> None:
 	img1 = Image.new("RGB", (100, 100), "red")
 	img2 = Image.new("RGB", (50, 50), "blue")
 	with pytest.raises(ValueError):
 		image_diff(img1, img2)
 
 
-def test_identical_rgba_vs_rgb():
+def test_identical_rgba_vs_rgb() -> None:
 	img1 = Image.new("RGBA", (100, 100), (255, 0, 0, 255))
 	img2 = Image.new("RGB", (100, 100), "red")
 	assert image_diff(img1, img2) == 0
